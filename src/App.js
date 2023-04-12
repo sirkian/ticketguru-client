27d45bcffd3e604a694738cb000dee0037fda4e9
Milla Laaja <millamlaaja@gmail.com>
2023-04-12 19:55:54 +0300

    client aloitettu #139

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TicketGuru Ticket sales
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Hae tapahtuma</p>
+        <p>Valitse lippu</p>
+        <p>Valitse määrä</p>
+        <p>Maksa tapahtuma</p>
+        <p>Merkitse myydyksi</p>
     </div>
   );
 }
